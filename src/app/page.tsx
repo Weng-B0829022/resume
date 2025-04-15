86a28cf98c60524203128339d402b616fabe72f8
翁 <dennis1989999@gmail.com>
2025-04-15 16:13:53 +0800

    AdditionalExperience del

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import Experience from './components/Experience';
 import Skills from './components/Skills';
 import Education from './components/Education';
 import Projects from './components/Projects';
-import AdditionalExperience from './components/AdditionalExperience';
 import Footer from './components/Footer';
 
 const Home = () => {
